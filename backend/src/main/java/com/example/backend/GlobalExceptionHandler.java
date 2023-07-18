8b19ce157cff21d76040b909b60bcfc706058a2d
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-07-18 22:07:12 +0200

    one left

--- backend/src/main/java/com/example/backend/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/backend/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.backend;
 
-import com.example.backend.exceptions.ApiError;
 import com.example.backend.exceptions.*;
 
 import org.springframework.dao.DataIntegrityViolationException;
@@ -15,33 +14,34 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({EntityNotFoundException.class, GradeNotFoundException.class, StudentNotEnrolledException.class})
-    public ResponseEntity<ApiError> handleEntityNotFoundException(EntityNotFoundException e) {
-        ApiError apiError = new ApiError(LocalDateTime.now(), Collections.singletonList(e.getMessage()));
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
-    }
-    @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity<Object> handleDataAccessException(EmptyResultDataAccessException ex) {
-        ApiError error = new ApiError(LocalDateTime.now(), List.of("Cannot delete non-existing resource"));
+    public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex) {
+        ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getMessage()));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Object> handleDataAccessException(EmptyResultDataAccessException ex) {
+        ErrorResponse error = new ErrorResponse(Arrays.asList("Cannot delete non-existing resource"));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        ApiError error = new ApiError(LocalDateTime.now(), List.of("Data Integrity Violation: we cannot process your request."));
+        ErrorResponse error = new ErrorResponse(Arrays.asList("Data Integrity Violation: we cannot process your request."));
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         List<String> errors = new ArrayList<>();
-        ex.getBindingResult().getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
-        return new ResponseEntity<>(new ApiError(LocalDateTime.now(), errors), HttpStatus.BAD_REQUEST);
+        ex.getBindingResult().getAllErrors().forEach((error)-> errors.add(error.getDefaultMessage()));
+        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
     }
 }
--- backend/src/main/java/com/example/backend/exceptions/EntityNotFoundException.java
+++ backend/src/main/java/com/example/backend/exceptions/EntityNotFoundException.java
@@ -5,5 +5,5 @@ public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(Long id, Class<?> entity) {
         super("The " + entity.getSimpleName().toLowerCase() + " with id '" + id + "' does not exist in our records");
     }
-    
+
 }
